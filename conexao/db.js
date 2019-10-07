da41b02120f2b756a6eebb919ec563a252bad17a
henriquemsr <henriquemendes25@gmail.com>
2019-10-07 18:43:35 -0300

    apiteste

--- conexao/db.js
+++ conexao/db.js
@@ -1,19 +1,18 @@
-module.exports = (app) =>{
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('acai','root','',{
-    port: '3306',
-    host: 'localhost',
-    dialect: 'mysql'
-   })
-let conect = false;
-//metodo para autenticar
-sequelize.authenticate().then(()=>{
-    conect = true;
-   }).catch((erro)=>{
-       conect = false;
-       console.log("erro ao conectar" + erro)
-   })
-
-   return [conect,sequelize];
+module.exports = (app) => {
+    const Sequelize = require('sequelize');
+    const sequelize = new Sequelize('acai', 'root', '', {
+        port: '3306',
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+    let conect = false;
+    //metodo para autenticar
+    sequelize.authenticate().then(() => {
+        conect = true;
+    }).catch((erro) => {
+        conect = false;
+        console.log("erro ao conectar" + erro)
+    })
+    return [conect, sequelize];
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const consign = require('consign');
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./src/index.html'));
+})
 
 consign()
 .include('conexao/db.js')
